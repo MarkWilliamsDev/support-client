812e301646710276bd7de84fc9008b27e1e93744
FastnBulbous81 <m.williams.entropy@gmail.com>
2021-07-19 10:48:16 +0100

    Fixed date display for messages

--- src/components/list/listItems/specific/GlobalMessageListItem.js
+++ src/components/list/listItems/specific/GlobalMessageListItem.js
@@ -1,10 +1,11 @@
+import DateDisplay from '@/components/display/elements/DateDisplay'
 import StringDisplay from '@/components/display/elements/StringDisplay'
 
 function GlobalMessageListItem({ item }) {
   return (
     <div>
       <hr />
-      <StringDisplay content={item.createdAt} />
+      <DateDisplay date={item.createdAt} />
       <StringDisplay content={item.message} />
       <StringDisplay content={item._id} />
     </div>
